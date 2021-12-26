d9e94e6a9be058213a97c043f27cd0af75b5198c
Stanley Cooper <scooperlu@gmail.com>
2021-12-26 17:30:53 -0500

    changes to file system

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,30 +1,60 @@
-import React, { useState } from "react";
-import { Switch, Route } from "react-router-dom";
-import Header from "./Header";
-import Home from "./Home/Home";
-import NotFound from "./NotFound";
+import React from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
+import Header from './Header';
+import NotFound from './NotFound';
+import Home from './Home';
+import StudyDeck from './StudyDeck';
+import CreateDeck from './CreateDeck';
+import ViewDeck from './ViewDeck';
+import EditDeck from './EditDeck';
+import AddCard from './AddCard';
+import EditCard from './EditCard';
 
 function Layout() {
-  const [deckLength, setDeckLength] = useState(0);
-  const updateDecks = (newDecks) => {
-    setDeckLength(() => deckLength + newDecks)
-  }
-  
   return (
-    <>
+    <React.Fragment>
       <Header />
-      <div className="container">
-        {/* TODO: Implement the screen starting here */}
-
+      <div className='container'>
         <Switch>
-          {/* HOME */}
-          <Route exact path="/">
-            <Home updateDecks={updateDecks} deckLength={deckLength} />
+          <Route exact path='/'>
+            <Link to='/decks/new'>
+              <button className='btn btn-secondary'>
+                <i className='fas fa-plus'></i> Create Deck
+              </button>
+            </Link>
+            <Home />
+          </Route>
+
+          <Route exact path='/decks/new'>
+            <CreateDeck />
+          </Route>
+
+          <Route exact path='/decks/:deckId/study'>
+            <StudyDeck />
+          </Route>
+
+          <Route exact path='/decks/:deckId/edit'>
+            <EditDeck />
+          </Route>
+
+          <Route exact path='/decks/:deckId'>
+            <ViewDeck />
+          </Route>
+
+          <Route exact path='/decks/:deckId/cards/new'>
+            <AddCard />
+          </Route>
+
+          <Route exact path='/decks/:deckId/cards/:cardId/edit'>
+            <EditCard />
+          </Route>
+
+          <Route>
+            <NotFound />
           </Route>
         </Switch>
-        <NotFound />
       </div>
-    </>
+    </React.Fragment>
   );
 }
 
